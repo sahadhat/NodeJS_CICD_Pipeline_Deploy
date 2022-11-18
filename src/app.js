ad1a00d398deac5af2c83ba3f2191bf1c25db701
DevDas <epratapdas@gmail.com>
2022-11-18 23:20:56 +0530

    Node applicayion on github action for NodeJS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+app.get('/', (req, res)=>{
+    res.status(200).send('Yes nodeJS enpoint worked');
+})
 app.get('/testNode', (req, res)=>{
     res.status(200).send('Yes nodeJS testNode enpoint worked');
 })
